0a0621058841fc198da9b22b4dd80dbdbd12a4c5
kirg2711 <n00399349@upn.pe>
2025-12-15 21:46:58 +0100

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,17 @@ self.addEventListener("install", event => {
       return cache.addAll([
         "./",
         "./index.html",
-        "./manifest.json"
+        "./manifest.json",
+        "./icon.png"
       ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
